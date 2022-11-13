ac99ddd0bb8bd8df7ecd345afa3e6f46737b08d1
Volodya Petrov <volodya.workflow@gmail.com>
2022-11-13 20:22:13 +0500

    Реализован простейший eventloop

--- 0/main.py
+++ 0/main.py
@@ -1,5 +1,8 @@
 import socket
+from select import select
 
+
+to_monitor = []
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,23 +11,37 @@ server_socket.listen()
 
 
 def accept_connection(server_socket):
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(addr)
+    client_socket, addr = server_socket.accept()
+    print(addr)
+    
+    to_monitor.append(client_socket)
 
 
 def send_message(client_socket):
+    request = client_socket.recv(4096)
+
+    if request:
+        response = "Hello, world\n".encode()
+        client_socket.send(response)
+    else:
+        client_socket.close()
+
+
+def event_loop():
     while True:
-        request = client_socket.recv(4096)
+        # мониторим файловые дескриптеры изменившихся сокетов
+        ready_to_read, _, _ = select(to_monitor, [], []) 
+        for sock in ready_to_read:
+            if sock is server_socket:
+                accept_connection(sock)
+            else:
+                send_message(sock)
 
-        if not request:
-            break
-        else:
-            response = "Hello, world\n".encode()
-            client_socket.send(response)
 
-    client_socket.close()
-    print("Connection cloes")
 
 if __name__ == '__main__':
-    accept_connection(server_socket)
+    to_monitor.append(server_socket)
+    event_loop()
+    #accept_connection(server_socket)
+
+
